3eec2acfa52cfaa4ffa2d3864dae50a826e1b518
Marc Brouwer <m.r.brouwer@student.tue.nl>
2015-05-27 14:27:43 +0200

    Recommit after conflict

--- src/gui/Anava.java
+++ src/gui/Anava.java
@@ -1,26 +1,5 @@
 package gui;
 
-<<<<<<< HEAD
-=======
-import dblis.SportData2;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
-
->>>>>>> origin/master
 /**
  *
  * @author Brouwer M.R.
