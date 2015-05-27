c7c00c5ea38ed7044619a8762d0c9da4bf1fd8f7
s130031 <s130031@S130031.campus.tue.nl>
2015-05-27 15:56:44 +0200

    Revert "BarChart gui option implemented"
    This reverts commit c15bd23d4cb0d152e999ecdb4ae8869595448f4c.

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -643,10 +643,7 @@ public class MainFrame extends CenterFrame {
     }//GEN-LAST:event_showComparisonBtnActionPerformed
 
     private void histogramBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_histogramBtnActionPerformed
-        setParameters();
-        PopFrame pf = new PopFrame(5);
-        pf.setVisible(true);
-        pf.toFront();
+        // TODO add your handling code here:
     }//GEN-LAST:event_histogramBtnActionPerformed
 
     private void addBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addBtnActionPerformed
@@ -676,7 +673,6 @@ public class MainFrame extends CenterFrame {
         SportData2.getInstance().setDates(getStartDate(), getEndDate());
         SportData2.getInstance().setSelected(getSports());
         SportData2.getInstance().setInterval(timeInterval);
-        SportData2.getInstance().setFootballSeperate(jCheckBox1.isSelected());
     }
     
     private Date getStartDate() {
